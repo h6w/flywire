97af9f0dd4195cbc6a31548b32f7572740e777ed
Tudor Holton <tudor@tudorholton.com>
2012-08-17 23:14:38 +1000

    Supporting files for 0.2

---
+++ installer_files/linux/fakeroot/usr/lib/flywire/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export CLASSPATH=$CLASSPATH:/usr/lib/JMF-2.1.1e/lib/jmf.jar:/usr/lib/JMF-2.1.1e/lib/ext/mp3plugin.jar:/usr/lib/libkrispi/libkrispi.jar
+padsp java -jar /usr/lib/flywire/Flywire.jar
